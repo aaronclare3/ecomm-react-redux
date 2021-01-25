9248a4b953a19068436bc7ba199daa4c3ff9f052
aaronclare3 <aaronclare88@gmail.com>
2021-01-24 20:17:49 -0800

    added filter list logic

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -6,10 +6,14 @@ import {getProducts, clearProducts} from '../redux/Actions/ProductActions';
 import ProductItem from './ProductItem';
 
 const ProductList = () => {
-    const [listSorted, setListSorted] = useState('');
-    const dispatch = useDispatch();
+    const [sortedList, setSortedList] = useState('');
+    const [filteredList, setFilteredList] = useState([]);
+    const [listActive, setListActive] = useState('');
+    const [userSearchFilter, setUserSearchFilter] = useState('');
+
     const allProducts = useSelector(state => state.getProducts);
     const {loading, products, error} = allProducts;
+    const dispatch = useDispatch();
 
     useEffect(() => {
         dispatch(getProducts());
@@ -17,42 +21,64 @@ const ProductList = () => {
         return () => dispatch(clearProducts());
     }, [dispatch])
 
-    const checkListSorted = listSorted => {
-        let sortedProducts = [];
-        if(listSorted === "asc"){
-            sortedProducts = products.sort((a,b) => a.title.localeCompare(b.title))
-        }else if(listSorted === "desc"){
-            sortedProducts = products.sort((a,b) => b.title.localeCompare(a.title))
-        }else if(listSorted === "price-low-high"){
-            sortedProducts = products.sort((a,b) => a.price-b.price)
-        }else if(listSorted === "price-high-low"){
-            sortedProducts = products.sort((a,b) => b.price-a.price)
-        }else{
+    const filterHandler = inputVal => {
+        setListActive("filter");
+        setUserSearchFilter(inputVal);
+        const updatedList = products.filter(item => {
+            // Can also use .startsWith here if you want
+            return item.title.toLowerCase().includes(inputVal.toLowerCase());
+        })
+        setFilteredList(updatedList);
+    }
+
+    const checkListSorted = (sortedList, listActive) => {
+        if(listActive === ""){
             return products.map(item => {
                 return <ProductItem item={item} key={item.id} />
             });
+        }else if(listActive === "sorted"){
+            let sortedProducts = [];
+            if(sortedList === "asc"){
+                sortedProducts = products.sort((a,b) => a.title.localeCompare(b.title))
+            }else if(sortedList === "desc"){
+                sortedProducts = products.sort((a,b) => b.title.localeCompare(a.title))
+            }else if(sortedList === "price-low-high"){
+                sortedProducts = products.sort((a,b) => a.price-b.price)
+            }else if(sortedList === "price-high-low"){
+                sortedProducts = products.sort((a,b) => b.price-a.price)
+            }
+            return sortedProducts.map(item => {
+                return <ProductItem item={item} key={item.id} />
+            });
+        }else if(listActive === "filter"){
+            return filteredList.map(item => {
+                return <ProductItem item={item} key={item.id} />
+            });
         }
-        return sortedProducts.map(item => {
-            return <ProductItem item={item} key={item.id} />
-        });;
+    }
+
+    const sortedHandler = (sortedType) => {
+        setListActive("sorted");
+        setSortedList(sortedType);
     }
 
     return (
         <div>
-                <span>Sort List </span>
             <div>
-                <span onClick={() => setListSorted("asc")}>A-Z </span>
+                <div><span>Filter List</span></div>
+                <div><input type="text" onChange={e => filterHandler(e.target.value)} value={userSearchFilter}/></div>
+
             </div>
             <div>
-                <span onClick={() => setListSorted("desc")}>Z-A </span>
+                <div><span>Sort List </span></div>
+                <div><span onClick={() => sortedHandler("asc")}>A-Z </span></div>
+                <div><span onClick={() => sortedHandler("desc")}>Z-A </span></div>
+                <div><span onClick={() => sortedHandler("price-low-high")}>Price: Low to High </span></div>
+                <div><span onClick={() => sortedHandler("price-high-low")}>Price: High to Low </span></div>
             </div>
             <div>
-                <span onClick={() => setListSorted("price-low-high")}>Price: Low to High </span>
+                {loading ? <h2>Loading...</h2> : error ? <h2>{error}</h2> : checkListSorted(sortedList, listActive)}
             </div>
-            <div>
-                <span onClick={() => setListSorted("price-high-low")}>Price: High to Low </span>
-            </div>
-            {loading ? <h2>Loading...</h2> : error ? <h2>{error}</h2> : checkListSorted(listSorted)}
         </div>
     )
 }
