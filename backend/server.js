c9abdefccb6619c3e2afc1365551565e0ec4305b
aaronclare3 <aaronclare88@gmail.com>
2021-02-01 10:37:51 -0800

    version 1.0.0

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const connectDB = require("./config/db");
 const productRoutes = require("./routes/productRoutes");
+const path = require("path");
 
 connectDB();
 
@@ -10,6 +11,16 @@ const app = express();
 app.use(express.json());
 
 app.use("/api/products", productRoutes);
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("frontend/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
