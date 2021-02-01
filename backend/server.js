7ab09b652ea5f5ff7f9917af74ca9a739fd51a74
aaronclare3 <aaronclare88@gmail.com>
2021-02-01 10:51:40 -0800

    changed port to 5000

--- backend/server.js
+++ backend/server.js
@@ -22,6 +22,6 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`server running on port ${PORT}`));
